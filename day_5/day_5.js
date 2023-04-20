80917f0c8bdebbb072f97fc90c252a5eb8441f65
Fred <fredrbina@gmail.com>
2023-04-20 16:30:21 -0400

    Ending the day_5 project

--- day_5/day_5.js
+++ day_5/day_5.js
@@ -3,13 +3,14 @@ let background_blur = document.querySelector(".fondo")
 
 load = 0;
 
-let interval = setInterval(blurring, 3)
+let interval = setInterval(blurring, 30)
 
 function blurring() {
-
-	load_num.innertext = `${load}%`;
-	background_blur.style.filter = `blur(${(100 - load)}px)`;
 	load++
+
+	load_num.innerText = `${load}%`;
+	load_num.style.opacity = `${(100 - load)}%`
+	background_blur.style.filter = `blur(${(100 - load)}px)`;
 	if( load > 99){
 		clearInterval(interval)
 	}
