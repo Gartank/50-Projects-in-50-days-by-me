2c1750503d14e490098127997fab9a2d093535bb
Fred Urbina <Fredrbina@gmail.com>
2026-04-06 13:11:12 -0400

    changing: whole project, re-thinking counters & adding an API-consuming idea form manage real user information and searching to display.

--- day_15/day_15.js
+++ day_15/day_15.js
@@ -1,26 +1,54 @@
-'use strict';
+
+const instagramUser = {
+    url : (user) => {
+        return `https://instagram-profile1.p.rapidapi.com/getprofile/${user}`
+    },
+    options :  {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'e4e22290afmshbe440e3822173cbp1bdef3jsn478dc373a896',
+            'x-rapidapi-host': 'instagram-profile1.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        }
+    }
+}
+const tiktokUser = {
+    undefined
+}
+const xUser = {
+    undefined
+}
+
 
 const countersContainer = document.querySelector( '.counters' );
 const counterArray = [];
-const dataDir = './data.json';
+const dataDir = './user-data.json';
+
 
 class Counter {
 
-    constructor( platform, userName = 'default_user', followers = 20000, logo = '', counterElem = null ){
-        
+    constructor( platform ){
         this.platform = platform;
-        this.logo = logo;
-        this.userName = userName;
-        this.followers = followers;
-        
-        this.elem = Counter.initElem( platform, userName, counterElem , followers, logo );
+        if(platform == 'instagram'){
+            this.platformApi =  instagramUser;
+        }
+        else if (platform == 'tiktok'){
+            this.platformApi =  tiktokUser;
+        }
+        else if (platform == 'x'){
+            this.platformApi = xUser;
+        }
+        this.logo = `../assets/${platform}-logo.svg`;
+        this.userName = 'user';
+        this.followers = '676767';
+        this.userPic = '../assets/user_male_circle.svg';
+
+        this.elem = Counter.initElem( this.logo );
+
+        this.refreshElem();
     }
-    
     delete(){
         this.elem.remove();
-        delete this.elem;
-        delete this.socialMedia;
-        delete this.followers;
         delete this;
     }
 
@@ -33,9 +61,12 @@ class Counter {
         if( actualValue < this.followers ){
             actualValue += Math.ceil(this.followers / 500);
             counter.innerText = actualValue;
+            let counterAddedDelay = 0;
+            counterAddedDelay+= actualValue > this.followers * 0.95 ? 50 : 0;
+
             setTimeout(() => {
                 this.startCounter(actualValue)
-            }, 1);
+            },counterAddedDelay )
         }
         else{
             counter.innerText = this.followers;
@@ -43,48 +74,131 @@ class Counter {
         }
     }
 
-    static initElem( platform, userName, counterElem , followers, logo ){
+    static initElem( logoUrl ){
         const elem = document.createElement('div');
-        elem.classList.add('counter', `counter-${platform.toLowerCase()}`)
-        
-        const logoElem = document.createElement('span');
-        logoElem.classList.add('counter-logo', `logo-${platform.toLowerCase()}`);
-        logoElem.style.backgroundImage = `url(${logo})`;
+        elem.classList.add('counter')
+
+        const titleContainer = document.createElement('div');
+        titleContainer.classList.add( 'counter-title' )
+
+        const logoElem = document.createElement('img');
+        logoElem.classList.add('counter-logo');
+        logoElem.src = `${logoUrl}`
 
         const title = document.createElement('h2');
         title.classList.add('counter-user');
-        title.innerText = `@${userName}`;
+        title.innerText = `@${this.userName}`;
+
+        titleContainer.append(logoElem, title);
+
+        const userPic = document.createElement('img');
+        userPic.classList.add('counter-pic');
+        userPic.src = `${this.userPic}`;
+
+        const searchBar = document.createElement('input');
+        searchBar.className = ('searchBar searchBar-input');
+        searchBar.type = 'text';
+        searchBar.placeholder = 'Username';
+
+        const searchBarBtn = document.createElement('button');
+        searchBarBtn.className = 'searchBar searchBar-btn';
+        searchBarBtn.innerText = '>';
+
+        const searchBarContainer = document.createElement('span')
+        searchBarContainer.className = 'searchBar searchBar-container';
+        searchBarContainer.appendChild(searchBar);
+        searchBarContainer.appendChild(searchBarBtn);
 
         const number = document.createElement('div');
         number.classList.add('counter-number');
         number.innerText = 0;
 
-        elem.appendChild(logoElem);
-        elem.appendChild(title);
+        elem.appendChild(userPic);
         elem.appendChild(number);
+        elem.appendChild(titleContainer);
+        elem.appendChild(searchBarContainer);
+
+        counterArray.forEach(counter => {
+            const searchBarbtn = counter.querySelector('.searchBar-btn');
+            const searchBarcontent = counter.querySelector('searchBar-input');
+
+            searchBarbtn.addEventListener('click', ev => {
+            console.log('yeah, it works');
+            })
+        });
 
         return elem;
-    } 
+    }
+
+    async refreshElem(){
+        const response = await fetch(dataDir);
+        const data = (await response.json())["socialMediaAccounts"];
+
+        const userData = data.filter( user => user.platformName == this.platform )[0];
+
+        this.userName = userData['username'];
+        this.followers = userData['followerCount'];
+        this.userPic = userData["userPic"];
+
+        this.elem.querySelector(".counter-user").innerText = this.userName;
+        this.elem.querySelector(".counter-number").innerText = this.followers;
+        this.elem.querySelector(".counter-pic").src = this.userPic;
+        this.elem.querySelector(".counter-logo").src = `${this.logo}`;
+    }
 }
 
 
-async function getData(dir = dataDir){
-    const response = await fetch(dir);
-    const data = await response.json();
-    const mediaAccounts = data["socialMediaAccounts"];
-
-    for( let account in mediaAccounts){
-        const accountAux = mediaAccounts[account];
-        
-        const counterAux = new Counter( accountAux['platformName'], accountAux['username'], accountAux['followerCount'], accountAux['logo']);
-        
-        counterArray.push(counterAux);
-        counterAux.deployElem();
-    }
+async function refreshCounters(dir = dataDir){
 
     counterArray.forEach(counter => {
-        counter.startCounter();
+        counter.refreshElem()
     });
 }
 
-getData()
+async function refreshDataUsers ( platform, object ) {
+    const response = await fetch(dataDir);
+    const data = (await response.json())["socialMediaAccounts"];
+
+    const userData = Object.entries(data).filter( ([key, value]) => key == 'platfotmName' && value == platform )
+
+    Object.keys(object).forEach( key => userData[key] = object[key] );
+}
+
+async function fetchUserInstagram( username ){
+    const userUrl = instagramUser.url(username);
+
+    try {
+        const response = await fetch(userUrl, instagramUser.options);
+        const result = await response.json();
+
+        const profileData = {
+            'followerCount' : result.followers,
+            'username' : result.username,
+            'userPic' : result.profile_pic_url
+        };
+
+        refreshDataUsers('instagram', profileData);
+    }
+    catch (error){
+        console.error(error);
+    }
+}
+
+async function fetchUserTiktok(  ){
+    undefined;
+}
+
+function initCounter( ){
+    const igCounter = new Counter("instagram" );
+    const tiktokCounter = new Counter("tiktok");
+    const XCounter = new Counter("x");
+
+    counterArray.push(igCounter, tiktokCounter, XCounter);
+    counterArray.forEach( counter => countersContainer.append(counter.elem) );
+}
+
+
+
+
+initCounter();
+refreshCounters()
